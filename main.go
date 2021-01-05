82ae6a4e0a6888305f5faa4ad87ad779edc1f627
Starlight Romero <starlightromero@gmail.com>
2021-01-04 23:09:23 -0800

    [update] added sudo opponent

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -35,5 +37,18 @@ func main() {
 		fmt.Println("Hit or Stay?")
 		fmt.Scanln(&input)
 	}
-
+	handValue := hand.value()
+	if handValue < 21 {
+		fmt.Println("You got", handValue)
+		rand.Seed(time.Now().Unix())
+		aiHandValue := rand.Intn(21)
+		fmt.Println("The AI got", aiHandValue)
+		if aiHandValue > handValue {
+			fmt.Println("You loose!")
+		} else if aiHandValue < handValue {
+			fmt.Println("You win!")
+		} else {
+			fmt.Println("No one won...")
+		}
+	}
 }
