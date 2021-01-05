26caf94d10ab8d56d09295e148369aa7bcc63fb4
Starlight Romero <starlightromero@gmail.com>
2021-01-04 22:55:23 -0800

    [update] built out basic game structure

--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	cards := newDeck()
-	cards.shuffle()
-	fmt.Println(cards)
-	hand, cards := cards.dealOne()
-	fmt.Println(hand)
+
+	n := 1
+	for n < 10 {
+		cards.shuffle()
+		n++
+	}
+
+	hand, cards := cards.dealTwo()
+	hand.print()
+
+	fmt.Println("Hit or Stay?")
+	var input string
+	fmt.Scanln(&input)
+	for strings.ToLower(input) == "hit" {
+		newCard, _ := cards.dealOne()
+		hand = append(hand, newCard...)
+		hand.print()
+		handValue := hand.value()
+		if handValue > 21 {
+			fmt.Println("You went over 21. You lose!")
+			break
+		} else if handValue == 21 {
+			fmt.Println("You got 21. You win!")
+			break
+		}
+		fmt.Println("Hit or Stay?")
+		fmt.Scanln(&input)
+	}
+
 }
